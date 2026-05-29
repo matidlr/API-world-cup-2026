69e8b32049ff8e59bd08638013fa08422a7b4456
matidlr <argentinavende.sa@gmail.com>
2026-05-29 00:34:46 -0300

    mannage errores on the backend service

--- backend/world-cup-core/src/match/services/play-final.service.ts
+++ backend/world-cup-core/src/match/services/play-final.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
 import { AdminService } from 'src/admin/admin.service';
 import { AbstractBaseService } from 'src/basic/abstract-base.service';
 import { CurrentWorldCupApiResponse } from '../model/play-final-service.interface';
 import { WorldCupFeatureApiService } from 'src/basic/world-cup-feature-api.service';
+import { WorldCupCoreErrorCode } from 'src/basic/model/world-cup-core-error-code.enum';
+import { ApiErrorMappingRule, ApiErrorStatusMap } from 'src/basic/error/error.utils';
+
+const SIMULATION_UNAVAILABLE_MESSAGE =
+  'World Cup simulation is not available yet. Run simulation first.';
+
+const SIMULATION_API_ERROR_STATUS_MAP: ApiErrorStatusMap = {
+  [HttpStatus.NOT_FOUND]: {
+    messageCode: WorldCupCoreErrorCode.WC_PLAY_FINAL_UNAVAILABLE,
+    message: SIMULATION_UNAVAILABLE_MESSAGE,
+    statusCode: HttpStatus.CONFLICT,
+  },
+  [HttpStatus.CONFLICT]: {
+    messageCode: WorldCupCoreErrorCode.WC_SIMULATION_UNAVAILABLE,
+    message: SIMULATION_UNAVAILABLE_MESSAGE,
+    statusCode: HttpStatus.CONFLICT,
+  },
+};
+
+const SIMULATION_API_ERROR_FALLBACK: ApiErrorMappingRule = {
+  messageCode: WorldCupCoreErrorCode.WC_SIMULATION_FETCH_FAILED,
+  message: 'Unable to load simulation data from World Cup API.',
+  statusCode: HttpStatus.BAD_GATEWAY,
+};
 
 @Injectable()
 export class PlayFinalService extends AbstractBaseService {
@@ -19,7 +43,7 @@ export class PlayFinalService extends AbstractBaseService {
       );
 
       if (!this.hasFinalists(worldCup)) {
-        throw this.si;
+        throw this.createSimulationUnavailableException();
       }
 
       return this.buildSimulationScreen(worldCup, resolvedLang);
@@ -36,6 +60,16 @@ export class PlayFinalService extends AbstractBaseService {
       worldCup?.finalAwayTeamName,
     );
   }
+
+  private createSimulationUnavailableException(): HttpException {
+      return new HttpException(
+        {
+          messageCode: WorldCupCoreErrorCode.WC_PLAY_FINAL_UNAVAILABLE,
+          message: SIMULATION_UNAVAILABLE_MESSAGE,
+        },
+        HttpStatus.CONFLICT,
+      );
+    }
 }
 
 
