dcd5465e539321a41acf724e630af0b32bd6fa2b
matidlr <argentinavende.sa@gmail.com>
2026-05-29 00:20:25 -0300

    ERROR

--- backend/world-cup-core/src/match/services/play-final.service.ts
+++ backend/world-cup-core/src/match/services/play-final.service.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { AdminService } from 'src/admin/admin.service';
 import { AbstractBaseService } from 'src/basic/abstract-base.service';
-import { WorldCupApiService } from 'src/basic/world-cup-api.service';
+import { CurrentWorldCupApiResponse } from '../model/play-final-service.interface';
+import { WorldCupFeatureApiService } from 'src/basic/world-cup-feature-api.service';
 
 @Injectable()
 export class PlayFinalService extends AbstractBaseService {
-  constructor( worldCupApiService: WorldCupApiService, 
+  constructor( private readonly worldCupfeatureApiService: WorldCupFeatureApiService, 
                adminService: AdminService
-  ){super(adminService, worldCupApiService)}
+  ){super(adminService)}
 
-  public async getCurrentFinal(): Promise<>{
-    
+  public async getCurrentFinal(lang?: string): Promise<CurrentWorldCupApiResponse>{
+    const resolvedLang = this.resolveLang(lang);
+    try {
+
+          const worldCup: CurrentWorldCupApiResponse = await this.worldCupfeatureApiService.getCurrentWorldCup(
+        resolvedLang,
+      );
+
+      if (!this.hasFinalists(worldCup)) {
+        throw this.si;
+      }
+
+      return this.buildSimulationScreen(worldCup, resolvedLang);
+    } catch (error) {
+      
+    }
+  }
+
+  private hasFinalists(worldCup: CurrentWorldCupApiResponse): boolean {
+    return Boolean(
+      worldCup?.finalHomeTeamId &&
+      worldCup?.finalHomeTeamName &&
+      worldCup?.finalAwayTeamId &&
+      worldCup?.finalAwayTeamName,
+    );
   }
 }
+
+
+ 
