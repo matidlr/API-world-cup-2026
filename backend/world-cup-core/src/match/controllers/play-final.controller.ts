20e7ec9d4930bf7898af30b784a2902fcb496d39
matidlr <argentinavende.sa@gmail.com>
2026-05-31 18:15:55 -0300

    play final controller

--- backend/world-cup-core/src/match/controllers/play-final.controller.ts
+++ backend/world-cup-core/src/match/controllers/play-final.controller.ts
@@ -1,9 +1,40 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags, ApiResponse } from '@nestjs/swagger';
 import { AbstractBaseService } from 'src/basic/abstract-base.service';
+import { AbstractController } from 'src/basic/abstract.controller';
+import { PlayFinalService } from '../services/play-final.service';
+import { PlayFinalModel, WorldCupStatusResponse } from '../services/play-final-service.model';
 
 @ApiTags('final-match')
 @Controller('final-match')
-export class PlayFinalController {
-  // TODO: Se debe implementar el controlador y el servicio correspondiente.
+export class PlayFinalController extends AbstractController {
+  constructor(private readonly playFinalService: PlayFinalService){super()}
+
+   @Get('current')
+     @ApiOperation({ summary: 'Play final- Cuurent final' })
+     @ApiQuery({ name: 'lang', required: false, enum: ['es', 'en'] })
+     public async getFinal(@Query('lang') lang?: string): Promise<unknown> {
+       return this.createOkResponse(await this.playFinalService.getWorldCupStatus(lang));
+     }
+
+  @Post('play-final')
+  @HttpCode(HttpStatus.OK) // Cambia el estado por defecto de un POST (21) a 200 OK
+  @ApiOperation({ summary: 'Play final - Start the final' })
+  @ApiQuery({ 
+    name: 'lang', 
+    required: false, 
+    enum: ['es', 'en'], 
+    description: 'Idioma para los comentarios y eventos del partido (por defecto "es")' 
+  })
+
+  @ApiResponse({ status: 200, description: 'La final se inició correctamente y devuelve los datos del partido.' })
+  
+  async playFinal(
+    @Query('lang') lang?: string 
+  ) {
+    return await this.playFinalService.startFinal(lang);
+  }
+
+
+
 }
